87cfe0f9e76098d2a17bce5f5d2d2206e2a1f915
Alexander Yusiuz <uzuz98@mail.ru>
2024-06-13 16:01:10 +0300

    mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -51,31 +51,33 @@ class ContactController extends Controller
                 'company' => $request->company,
             ]);
 
-            // Создание экземпляра PHPMailer
-            $mail = new PHPMailer(true);
+            // // Создание экземпляра PHPMailer
+            // $mail = new PHPMailer(true);
 
-            // Включение режима отладки "verbose"
-            $mail->SMTPDebug = 2; // Можно установить любой уровень отладки (0 - без отладки, 1 - базовая отладка, 2 - подробная отладка)
+            // // Включение режима отладки "verbose"
+            // $mail->SMTPDebug = 2; // Можно установить любой уровень отладки (0 - без отладки, 1 - базовая отладка, 2 - подробная отладка)
 
-            // Настройка сервера и отправки письма
-            $mail->isSMTP();
-            $mail->Host = env('MAIL_HOST'); // Укажите адрес вашего SMTP-сервера
-            $mail->SMTPAuth = true;
-            $mail->Username = env('MAIL_USERNAME'); // Укажите свой адрес электронной почты
-            $mail->Password = env('MAIL_PASSWORD'); // Укажите пароль от вашей почты
-            $mail->SMTPSecure = env('MAIL_ENCRYPTION');
-            $mail->Port = env('MAIL_PORT');
+            // // Настройка сервера и отправки письма
+            // $mail->isSMTP();
+            // $mail->Host = env('MAIL_HOST'); // Укажите адрес вашего SMTP-сервера
+            // $mail->SMTPAuth = true;
+            // $mail->Username = env('MAIL_USERNAME'); // Укажите свой адрес электронной почты
+            // $mail->Password = env('MAIL_PASSWORD'); // Укажите пароль от вашей почты
+            // $mail->SMTPSecure = env('MAIL_ENCRYPTION');
+            // $mail->Port = env('MAIL_PORT');
 
-            $mail->setFrom('from@example.com', 'Your Name'); // Укажите адрес отправителя
-            $mail->addAddress(setting('site.email_to_address') ?? config('services.mail.from_address'));
+            // $mail->setFrom('from@example.com', 'Your Name'); // Укажите адрес отправителя
+            // $mail->addAddress(setting('site.email_to_address') ?? config('services.mail.from_address'));
 
-            // Установка контента письма
-            $mail->isHTML(true);
-            $mail->Subject = 'Subject';
-            $mail->Body = 'Message body';
+            // // Установка контента письма
+            // $mail->isHTML(true);
+            // $mail->Subject = 'Subject';
+            // $mail->Body = 'Message body';
 
-            // Отправка письма
-            $mail->send();
+            // // Отправка письма
+            // $mail->send();
+
+            Mail::to(setting('site.email_to_address') ?? config('services.mail.from_address'))->send(new ContactMessage($contact));
 
             return redirect()->back()->with('success', 'Your message has been submitted successfully');
         } catch (Exception $e) {
