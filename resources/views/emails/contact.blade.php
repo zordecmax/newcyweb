d2d1599255962fe46873992eba099f24d05cb747
Alexander Yusiuz <uzuz98@mail.ru>
2024-06-13 16:34:20 +0300

    feat:add message and company

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -2,5 +2,10 @@
     <h3>New Contact Message</h3>
     <p>Name: {{ $contact->name }}</p>
     <p>Phone: {{ $contact->phone }}</p>
-    <p>Subject: {{ $contact->subject }}</p>
+    @if ($contact->company)
+        <p>Company: {{ $contact->company }}</p>
+    @endif
+    @if ($contact->message)
+        <p>Message: {{ $contact->message }}</p>
+    @endif
 </div>
