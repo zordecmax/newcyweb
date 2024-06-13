c1d5addf4cc2e3da9087491edb6670feb0f5a4ac
Alexander Yusiuz <uzuz98@mail.ru>
2024-06-13 14:57:09 +0300

    f

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -19,27 +19,6 @@ class ContactController extends Controller
         return view('pages.contacts');
     }
 
-    // public function store(ContactRequest $request)
-    // {
-    //     try {
-    //         $contact = Contact::create([
-    //             'name' => $request->name,
-    //             'phone' => $request->phone,
-    //             'email' => $request->email,
-    //             'message' => $request->message,
-    //             'company' => $request->company,
-    //         ]);
-
-    //         Mail::to(setting('site.email_to_address') ?? config('services.mail.from_address'))->send(new ContactMessage($contact));
-
-    //         return redirect()->back()->with('success', 'Your message has been submitted successfully');
-    //     } catch (ValidationException $e) {
-    //         dd($e);
-    //         $errorMessage = $e->getMessage() ?? "Error, Try again!";
-    //         return redirect()->back()->with('error', $errorMessage);
-    //     }
-    // }
-
     public function store(ContactRequest $request)
     {
         try {
@@ -51,36 +30,31 @@ class ContactController extends Controller
                 'company' => $request->company,
             ]);
 
-            // Создание экземпляра PHPMailer
-            $mail = new PHPMailer(true);
-
-            // Включение режима отладки "verbose"
-            $mail->SMTPDebug = 2; // Можно установить любой уровень отладки (0 - без отладки, 1 - базовая отладка, 2 - подробная отладка)
-
-            // Настройка сервера и отправки письма
-            $mail->isSMTP();
-            $mail->Host = env('MAIL_HOST'); // Укажите адрес вашего SMTP-сервера
-            $mail->SMTPAuth = true;
-            $mail->Username = env('MAIL_USERNAME'); // Укажите свой адрес электронной почты
-            $mail->Password = env('MAIL_PASSWORD'); // Укажите пароль от вашей почты
-            $mail->SMTPSecure = env('MAIL_ENCRYPTION');
-            $mail->Port = env('MAIL_PORT');
-
-            $mail->setFrom('from@example.com', 'Your Name'); // Укажите адрес отправителя
-            $mail->addAddress(setting('site.email_to_address') ?? config('services.mail.from_address'));
-
-            // Установка контента письма
-            $mail->isHTML(true);
-            $mail->Subject = 'Subject';
-            $mail->Body = 'Message body';
-
-            // Отправка письма
-            $mail->send();
+            Mail::to(setting('site.email_to_address') ?? config('services.mail.from_address'))->send(new ContactMessage($contact));
 
             return redirect()->back()->with('success', 'Your message has been submitted successfully');
-        } catch (Exception $e) {
+        } catch (ValidationException $e) {
+            dd($e);
             $errorMessage = $e->getMessage() ?? "Error, Try again!";
             return redirect()->back()->with('error', $errorMessage);
         }
     }
+
+    // public function store(ContactRequest $request)
+    // {
+    //     try {
+    //         $contact = Contact::create([
+    //             'name' => $request->name,
+    //             'phone' => $request->phone,
+    //             'email' => $request->email,
+    //             'message' => $request->message,
+    //             'company' => $request->company,
+    //         ]);
+
+    //         return redirect()->back()->with('success', 'Your message has been submitted successfully');
+    //     } catch (Exception $e) {
+    //         $errorMessage = $e->getMessage() ?? "Error, Try again!";
+    //         return redirect()->back()->with('error', $errorMessage);
+    //     }
+    // }
 }
