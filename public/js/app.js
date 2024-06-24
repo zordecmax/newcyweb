bc5ed1c6564a7d5ca9cdf5ff734ba8bba1a1c841
alexutm123 <pilich.sasha@gmail.com>
2024-06-24 16:58:05 +0300

    fix:faq update to bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -27830,19 +27830,6 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/scss/faq.scss":
-/*!*********************************!*\
-  !*** ./resources/scss/faq.scss ***!
-  \*********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ }),
-
 /***/ "./resources/scss/home/index.scss":
 /*!****************************************!*\
   !*** ./resources/scss/home/index.scss ***!
@@ -28185,8 +28172,7 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
 /******/ 			"css/app": 0,
-/******/ 			"css/home/index": 0,
-/******/ 			"css/faq": 0
+/******/ 			"css/home/index": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -28236,10 +28222,9 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app","css/home/index","css/faq"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/app","css/home/index","css/faq"], () => (__webpack_require__("./resources/scss/app.scss")))
-/******/ 	__webpack_require__.O(undefined, ["css/app","css/home/index","css/faq"], () => (__webpack_require__("./resources/scss/faq.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","css/home/index","css/faq"], () => (__webpack_require__("./resources/scss/home/index.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/home/index"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/home/index"], () => (__webpack_require__("./resources/scss/app.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","css/home/index"], () => (__webpack_require__("./resources/scss/home/index.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
