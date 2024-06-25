eda6523320afd00c3adf38db8fb8be5ffbc83dc6
alexutm123 <pilich.sasha@gmail.com>
2024-06-25 14:39:28 +0300

    feat:price

--- resources/views/components/home/page-prices.blade.php
+++ resources/views/components/home/page-prices.blade.php
@@ -1,84 +1,12 @@
 <section class="prices py-5">
     <div class="container">
-    <h2 class="text-center mb-5">{{ $page->title }}</h2>
-    <div>{!! $page->body !!}</div>
-  
+        <h2 class="text-center mb-5">Prices</h2>
+          @if (empty($page->body))
+            <p class="text-center">There is no information on pricing.</p>
+          @else
+            <div>
+              {!! $page->getTranslatedAttribute('body', app()->getLocale(), 'fallback_locale') !!}
+            </div>
+          @endif
     </div>
 </section>
-<section class="prices py-5">
-        <div class="container">
-
-            <!-- Вкладки для выбора вариантов -->
-            <ul class="nav nav-tabs" id="priceTab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" id="price1-tab" data-toggle="tab" href="#price1" role="tab" aria-controls="price1" aria-selected="true">Вариант 1</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="price2-tab" data-toggle="tab" href="#price2" role="tab" aria-controls="price2" aria-selected="false">Вариант 2</a>
-                </li>
-            </ul>
-
-            <!-- Контент вкладок -->
-            <div class="tab-content" id="priceTabContent">
-                <div class="tab-pane fade show active" id="price1" role="tabpanel" aria-labelledby="price1-tab">
-                    <div id="price-result-1" class="s91 price-result">
-                        <h6>Сайты на основе премиум шаблонов и WordPress CMS</h6>
-                        <table class="s78 s78-b1 s78-m1">
-                            <tbody>
-                                <tr>
-                                    <td>Сайт-визитка:</td>
-                                    <td>от 300&euro;</td>
-                                </tr>
-                                <tr>
-                                    <td>Корпоративный сайт:</td>
-                                    <td>от 500&euro;</td>
-                                </tr>
-                                <tr>
-                                    <td>Landing Page</td>
-                                    <td>от 500&euro;</td>
-                                </tr>
-                                <tr>
-                                    <td>Интернет-магазин</td>
-                                    <td>от 600&euro;</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <span class="s80">Цена включает выбор шаблона, установку, настройку, замену логотипа, цветовой схемы, изображений, текстов(согласно оговоренному объему), настройку системы ускорения, установку дополнительной системы защиты.</span>
-                        <p><strong>Приемущества:</strong>&nbsp;Высокая скорость разработки, низкая цена, выбор из тысяч вариантов готового дизайна.</p>
-                        <p><strong>Недостатки:</strong>&nbsp;Низкая скорость работы сайта и как следствие трудности с выводом сайта в топ поисковых систем, необходимость в более дорогом хостинге, гораздо более дорогое дальнеешее техническое обслуживание.</p>
-                    </div>
-                </div>
-                <p><strong><span class="s80 dbn">Данное предложение не является публичной офертой и создано для ознакомления с примерным уровнем цен на типовые проекты. Конечная цена формируется в зависимости от конкретного объема работ, только после составления технического задания и может отличаться в большую или меньшую сторону.</span></strong></p>
-
-                <div class="tab-pane fade" id="price2" role="tabpanel" aria-labelledby="price2-tab">
-                    <div id="price-result-2" class="s91 price-result">
-                        <h6>Сайты на основе премиум шаблонов и WordPress CMS</h6>
-                        <table class="s78 s78-b1 s78-m1">
-                            <tbody>
-                                <tr>
-                                    <td>Сайт-визитка:</td>
-                                    <td>от 1300&euro;</td>
-                                </tr>
-                                <tr>
-                                    <td>Корпоративный сайт:</td>
-                                    <td>от 1500&euro;</td>
-                                </tr>
-                                <tr>
-                                    <td>Landing Page</td>
-                                    <td>от 1500&euro;</td>
-                                </tr>
-                                <tr>
-                                    <td>Интернет-магазин</td>
-                                    <td>от 1600&euro;</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <span class="s80">Цена включает выбор шаблона, установку, настройку, замену логотипа, цветовой схемы, изображений, текстов(согласно оговоренному объему), настройку системы ускорения, установку дополнительной системы защиты.</span>
-                        <p><strong>Приемущества:</strong>&nbsp;Высокая скорость разработки, низкая цена, выбор из тысяч вариантов готового дизайна.</p>
-                        <p><strong>Недостатки:</strong>&nbsp;Низкая скорость работы сайта и как следствие трудности с выводом сайта в топ поисковых систем, необходимость в более дорогом хостинге, гораздо более дорогое дальнеешее техническое обслуживание.</p>
-                    </div>
-                </div>
-                <p><strong><span class="s80 dbn">Данное предложение не является публичной офертой и создано для ознакомления с примерным уровнем цен на типовые проекты. Конечная цена формируется в зависимости от конкретного объема работ, только после составления технического задания и может отличаться в большую или меньшую сторону.</span></strong></p>
-            </div>
-        </div>
-    </section>
