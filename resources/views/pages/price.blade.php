2638549fb98cbddb6bebfdd484a80218f2d3af0a
alexutm123 <pilich.sasha@gmail.com>
2024-06-26 16:37:49 +0300

    fix

--- resources/views/pages/price.blade.php
+++ resources/views/pages/price.blade.php
@@ -12,19 +12,13 @@
 @section('content')
 <section class="hero hero-section-margin-top">
         <div class="container">
-            <div class="d-flex align-items-center justify-content-center">
-                <div class="hero-content text-center">
                   <h2 class="text-center mb-4">{{__('main.price')}}</h2>
                     @if (empty($page->body))
                      <p class="text-center">There is no information on pricing.</p>
                     @else
-                     <div>
-                      {!! $page->getTranslatedAttribute('body', app()->getLocale(), 'fallback_locale') !!}
-                     </div>
-                    @endif
-                </div>
-            </div>
-        </div>
+                     {!! $page->getTranslatedAttribute('body', app()->getLocale(), 'fallback_locale') !!}
+                 @endif
+           </div>
     </section>
     <x-contact-block />
 @endsection
