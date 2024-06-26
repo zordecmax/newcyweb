e505c8953749c6d852f252dd2aa1d9238825eb9a
alexutm123 <pilich.sasha@gmail.com>
2024-06-26 09:19:41 +0300

    fix

--- resources/views/components/home/page-prices.blade.php
+++ resources/views/components/home/page-prices.blade.php
@@ -1,57 +1,12 @@
 <section class="prices py-5">
-<div class="container">
-        <h2 class="text-center">Цена на наши услуги в Кишиневе</h2>
-        <div class="d-flex justify-content-center">
-        <ul class="list-group mt-4" style="width: 600px;">
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="pe-5">
-                01/ <span class="fs-5 fw-bold">Разработка сайта каталога</span>
-                </div>
-                <span class="">от 599€/ от 8 дней</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="pe-5">
-                02/ <span class="fs-5 fw-bold">Разработка веб приложений</span>
-                </div>
-                <span class="">от 299€/ от 5 дней</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="pe-5">
-                03/ <span class="fs-5 fw-bold">Разработка интернет магазинов</span>
-                </div>
-                <span class="">от 599€/ от 9 дней</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="pe-5">
-                04/ <span class="fs-5 fw-bold">Разработка сайтов визиток</span>
-                </div>
-                <span class="">от 299€/ от 7 дней</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="pe-5">
-                05/ <span class="fs-5 fw-bold">Заказать сайт landing Page</span>
-                </div>
-                <span class="">от 299€/ от 5 дней</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="pe-5">
-                06/ <span class="fs-5 fw-bold">Разработка приложений</span>
-                </div>
-                <span class="ms-4">от 899€/ от 22 дней</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="pe-5">
-                07/ <span class="fs-5 fw-bold">SEO продвижение</span>
-                </div>
-                <span class="ms-5">от 199€/ месяц</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-              <div class="pe-5">
-                08/ <span class="fs-5 fw-bold">Продвижение в социальных сетях</span>
-                </div>
-                <span class="me-2 m-sm-0">от 149€/ месяц</span>
-            </li>
-        </ul>
-       </div>
+ <div class="container">
+   <h2 class="text-center mb-5">Prices</h2>
+          @if (empty($page->body))
+            <p class="text-center">There is no information on pricing.</p>
+          @else
+            <div>
+              {!! $page->getTranslatedAttribute('body', app()->getLocale(), 'fallback_locale') !!}
+            </div>
+          @endif
     </div>
 </section>
